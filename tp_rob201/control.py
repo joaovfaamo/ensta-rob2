1e8f14ffd77062900a2e3f5ba0cb7569ead0e118
joaovfaamo <joao-victor.ferro@ensta.fr>
2026-05-04 17:10:11 +0200

    Tp 4 Finalizado com Extensao (Mudando Baseado no Angulo)

--- tp_rob201/control.py
+++ tp_rob201/control.py
@@ -22,8 +22,14 @@ def reactive_obst_avoid(lidar):
     else:
         speed = 0.3  # Reduzindo a velocidade para uma reação mais suave
         rotation_speed = 0
-    """
 
+    command = {"forward": speed,
+               "rotation": rotation_speed}
+    
+    return command
+
+    """
+    """
 # Second way: See only the front Secteur
 
     laser_dist = lidar.get_sensor_values()
@@ -50,7 +56,38 @@ def reactive_obst_avoid(lidar):
         speed = 0.3
         rotation_speed = 0
 
-   
+    command = {"forward": speed,
+               "rotation": rotation_speed}
+    
+    return command
+    """
+
+    #Third Way: Extension Possible: Change la direction base on the angle
+    #Verify the direction whose objet is the closest, and turn in the opposite direction
+
+    laser_dist = lidar.get_sensor_values()
+
+    num_rays = len(laser_dist) #mesure the number of rays in the lidar
+    mid_point = num_rays // 2 #the middle index of the lidar rays, assuming the front is in the middle of the array (180 DEGRES)
+    num_rays_side = 50 # (180 - 50) = 130 e (180 + 50) = 230, so we will look at the rays from index 130 to 230, which correspond to the front sector of the robot
+    
+    start_index = max(0, mid_point - num_rays_side) #130
+    end_index = min(num_rays, mid_point + num_rays_side) #230
+
+    left_sector = laser_dist[start_index:mid_point] #Copy the desired elements of the lidar vector to the new vector (left sect)
+    right_sector = laser_dist[mid_point:end_index] #Copy the desired elements of the lidar vector to the new vector (right sect)
+
+    SAFE_DISTANCE = 30
+    if ((left_sector.min() < right_sector.min()) and (left_sector.min() < SAFE_DISTANCE)): #If the closest object is on the left, turn right
+        speed = 0
+        rotation_speed = random.uniform(0.7, 1.0) * 1 #Turn right
+    elif ((right_sector.min() < left_sector.min()) and (right_sector.min() < SAFE_DISTANCE)): #If the closest object is on the right, turn left
+        speed = 0
+        rotation_speed = random.uniform(0.7, 1.0) * -1 #Turn left
+    else: #If there is no object in front, go forward
+        speed = 0.3
+        rotation_speed = 0
+
     command = {"forward": speed,
                "rotation": rotation_speed}
 
