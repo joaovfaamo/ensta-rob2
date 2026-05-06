2e8b10b45127eff74a3a9eddd651ee31d2db9746
joaovfaamo <joao-victor.ferro@ensta.fr>
2026-05-06 19:13:39 +0200

    TP4 Iniciado (Ainda n TEstado)

--- tp_rob201/tiny_slam.py
+++ tp_rob201/tiny_slam.py
@@ -58,7 +58,33 @@ class TinySlam:
         pose : [x, y, theta] nparray, corrected pose in world coordinates
         """
         # TODO for TP3
+        
+        self.__init__(OccupancyGrid(self.grid.x_max_map, self.grid.y_max_map, self.grid.resolution)) #Reinitialise the map to be empty (with all values at 0) at each iteration, so we can add the new points of the lidar at each iteration and have a more accurate map of the environment
+    
 
+
+        #Pega a posicao atual do robo
+        current_pose = pose
+        laser_dist = lidar.get_sensor_values()
+        laser_angles = np.linspace(-np.pi, np.pi, len(laser_dist)) 
+        obs_angles_world = laser_angles + current_pose[2]  # Angles of obstacles in the world frame
+        obs_x = current_pose[0] + laser_dist * np.cos(obs_angles_world)  # X coordinates of obstacles in the world frame
+        obs_y = current_pose[1] + laser_dist * np.sin(obs_angles_world)  # Y coordinates of obstacles in the world frame
+        qobs = np.vstack((obs_x, obs_y)).T  # Combine obs_x and obs_y into a single array of shape (num_obstacles, 2)
+
+        
+        self.grid.add_map_points(obs_x, obs_y, val=1) #Adiciona os pontos de obstaculos no mapa (com valor 1, ou seja, mais provavel de ser ocupado) 
+        self.grid.add_map_points(current_pose[0], current_pose[1], val=-1) #Adiciona a posicao do robo no mapa (com valor -1, ou seja, mais provavel de ser livre)
+        self.add_value_along_line(current_pose[0], current_pose[1], obs_x, obs_y, val=-1) #Adiciona os pontos entre a posicao do robo e os pontos de obstaculos no mapa (com valor -1, ou seja, mais provavel de ser livre)
+        self.conv_world_to_map(obs_x, obs_y)
+
+    
+
+
+
+
+
+        
     def compute(self):
         """ Useless function, just for the exercise on using the profiler """
        
