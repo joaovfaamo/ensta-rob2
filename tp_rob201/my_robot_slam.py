fa22a85bece552af294a73c1e2effe54352d50b4
joaovfaamo <joao-victor.ferro@ensta.fr>
2026-05-08 16:28:05 +0200

    TP3 Finalizado

--- tp_rob201/my_robot_slam.py
+++ tp_rob201/my_robot_slam.py
@@ -50,7 +50,21 @@ class MyRobotSlam(RobotAbstract):
         """
         Main control function executed at each time step
         """
-        return self.control_so_para_teste()
+
+        # 1. Mise à jour de la carte
+        self.tiny_slam.update_map(self.lidar(), self.odometer_values())
+
+        # 2. Incrementa o contador
+        self.counter += 1
+
+        # 3. Affichage (1 vez a cada 10)
+        if self.counter % 10 == 0:
+            # Exibe o mapa original de probabilidades diretemente, permitindo ver o degrade de log-odds
+            # sem forcar hard limits (seuillage visual)
+            self.occupancy_grid.display_cv(self.odometer_values())
+
+
+        return self.control_tp2()
 
     def control_tp1(self):
         """
@@ -69,41 +83,11 @@ class MyRobotSlam(RobotAbstract):
         Main control function with full SLAM, random exploration and path planning
         """
         pose = self.odometer_values()
-        goal = [-300, -300, 3.14/2 ]
+        goal = [-300, -400, 3.14/2 ]
 
         # Compute new command speed to perform obstacle avoidance
         command = potential_field_control(self.lidar(), pose, goal)
 
         return command
 
-    def control_so_para_teste(self):
-        """
-        Criei esta funcao para ter um controle que nao faz nada, para poder testar o SLAM e o mapeamento sem o robot se mover, e assim verificar se o mapa esta sendo atualizado corretamente.
-        Posso excluir ela depois
-        """
-        # 1. Mise à jour de la carte
-        self.tiny_slam.update_map(self.lidar(), self.odometer_values())
-
-        # 2. Incrementa o contador
-        self.counter += 1
-
-        # 3. Affichage (1 vez a cada 10)
-        if self.counter % 10 == 0:
-            # Salva o mapa original
-            original_map = np.copy(self.occupancy_grid.occupancy_map)
-            
-            # Aplica o seuillage para visualização (4 para parede, -4 para livre)
-            clean_map = np.zeros_like(original_map)
-            clean_map[original_map > 2] = 4
-            clean_map[original_map < -2] = -4
-            
-            # Coloca o mapa limpo na grid temporariamente e exibe
-            self.occupancy_grid.occupancy_map = clean_map
-            self.occupancy_grid.display_cv(self.odometer_values())
-            
-            # Restaura o mapa original de probabilidades
-            self.occupancy_grid.occupancy_map = original_map
-
-        command = {"forward": 0,
-                   "rotation": 0}
-        return command
+  
--- tp_rob201/tiny_slam.py
+++ tp_rob201/tiny_slam.py
@@ -69,26 +69,21 @@ class TinySlam:
         obs_y = current_pose[1] + laser_dist * np.sin(obs_angles_world)  # Y coordinates of obstacles in the world frame
         
         #A logica aqui é deixar um raio de incerteza ao redor do ponto de impacto do laser (evita instabilidade)
-        free_dist = laser_dist - 0.2  # Diminuí 20cm do limite do impacto (Ajuste o valor aqui dependendo da estabilidade) 
+        free_dist = laser_dist - 0.1  # Diminou para 10cm - Margem menor para paredes ficarem mais vermelhas (mais concentradas)
         free_dist = np.maximum(free_dist, 0) #Mantem os valores em positivos
 
          # Coordenadas Seguras para marcar como livres (um pouco antes do impacto)
         free_x = current_pose[0] + free_dist * np.cos(obs_angles_world)
         free_y = current_pose[1] + free_dist * np.sin(obs_angles_world)
 
-        #Adiciona 1 as celulas dos obstaculos e -1 as celulas em linha reta entre os dois e a posicao do robo
-        self.grid.add_map_points(obs_x, obs_y, val=1) #Adiciona os pontos de obstaculos no mapa (com valor 1, ou seja, mais provavel de ser ocupado) 
-        self.grid.add_map_points(np.array([current_pose[0]]), np.array([current_pose[1]]), val=-1) #Adiciona a posicao do robo no mapa (com valor -1, ou seja, mais provavel de ser livre)
+        # Adicionando um peso mais alto para a parede (bateu = +2 em vez de +1)
+        self.grid.add_map_points(obs_x, obs_y, val=2) #Adiciona os pontos de obstaculos no mapa 
+        self.grid.add_map_points(np.array([current_pose[0]]), np.array([current_pose[1]]), val=-1) 
         
-       #Adciona -1 para os pontos entre o robo e a posicao segura de impacto
        # Como o metodo aceita só valores unicos, usa o for para adicionar os pontos 
         for i in range(len(free_x)):
-             self.grid.add_value_along_line(current_pose[0], current_pose[1], free_x[i], free_y[i], val=-0.5)
+             self.grid.add_value_along_line(current_pose[0], current_pose[1], free_x[i], free_y[i], val=-0.2)
 
         self.grid.occupancy_map = np.clip(self.grid.occupancy_map, -5, 5)
-
-
-
-    def compute(self):
-        """ Useless function, just for the exercise on using the profiler """
-       
+        
+ 
